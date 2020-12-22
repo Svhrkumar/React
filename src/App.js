32d9a120d60ee1b796a684b14e21d6536250586b
svhrkumar544 <52437024+svhrkumar544@users.noreply.github.com>
2020-12-22 12:31:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+function App() {
+	return (
+    <div className='grid-container'>
+    <BrowserRouter>
+			<header>
+				<Link to='/'>React Shopping Cart</Link>
+				<Link to='/admin'>Admin</Link>
+			</header>
+			<main>
+			list
+			</main>
+      <footer>All right is reserved.</footer>
+      </BrowserRouter>
+		</div>
+	);
+}
+
+export default App;
