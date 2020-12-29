c4616d74abb7c20f757d3a5f8c4a8586a2e3f63a
svhrkumar544 <52437024+svhrkumar544@users.noreply.github.com>
2020-12-29 16:59:45 +0530

    added filter component functianality

--- src/App.js
+++ src/App.js
@@ -2,8 +2,32 @@ import React, { useState } from 'react';
 import Product from './data.json';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Products from './components/Products';
+import Filter from './components/Filter';
 function App() {
 	const [product, setProduct] = useState(Product.products);
+	const [size, setSize] = useState("")
+	const [sort, setSort] = useState("")
+	const filterProduct = (event) => {
+		console.log("---size----", event.target.value)
+		if (event.target.value === "") {
+			setProduct(Product.products)
+			setSize(event.target.value)
+		}
+		else {
+			setSize(event.target.value)
+			setProduct(Product.products.filter(item => item.availableSizes.indexOf(event.target.value) >= 0))
+		}
+	}
+	const sortProduct = (event) => {
+		console.log("---sort----", event.target.value)
+		const sorted = event.target.value
+		setSort(sorted)
+		setProduct(product.slice().sort((a, b) => (
+			sort === "lowest" ? ((a.price < b.price) ? 1 : -1) : sort === "highest" ? ((a.price > b.price) ? 1 : -1) : ((a._id > b._id) ? 1 : -1)
+		)))
+
+	}
+
 	return (
 		<div className='grid-container'>
 			<BrowserRouter>
@@ -14,6 +38,7 @@ function App() {
 				<main>
 					<div className='content'>
 						<div className='main'>
+							<Filter count={product.length} size={size} sort={sort} filterProducts={filterProduct} sortProducts={sortProduct} />
 							<Products products={product} />
 						</div>
 						<div className='sidebar'></div>
