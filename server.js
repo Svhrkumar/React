13779aa598dd5f81a417d6d3f53252b43b28762f
svhrkumar544 <52437024+svhrkumar544@users.noreply.github.com>
2021-01-07 13:56:16 +0530

    product backend done

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const shortid = require("shortid")
+
+const app = express()
+app.use(bodyParser.json())
+
+mongoose.connect("mongodb://localhost/react-shopping-cart",{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+})
+
+const Product = mongoose.model("product",new mongoose.Schema({
+    _id:{type: String,default:shortid.generate},
+    title:String,
+    description:String,
+    price:Number,
+    availableSizes:[String],
+    image:String,
+
+
+}))
+
+app.get("/api/products/",async (req,res)=>{
+    const products = await Product.find({})
+    res.send(products)
+
+})
+
+app.post("/api/products/",async (req,res)=>{
+    const newProduct = new Product(req.body)
+    const savedProduct = await newProduct.save()
+    res.send(savedProduct)
+})
+
+app.delete("/api/product/:id", async(req,res)=>{
+    const deltedProduct = await Product.findByIdAndDelete(req.params.id)
+    res.send(deltedProduct)
+})
+const port = process.env.PORT || 5000
+app.listen(port,() => console.log(`server at ${port}` ))
